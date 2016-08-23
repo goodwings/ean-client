4810b3d67cc48396e88e5dfa0e629de15278b511
lightowl <tc@goodwings.com>
2016-08-23 16:48:24 +0200

    Set special response codes on ean error

--- src/Subscriber/EanError.php
+++ src/Subscriber/EanError.php
@@ -38,7 +38,31 @@ class EanError implements SubscriberInterface
             $e->setCategory((string) $eanError->category);
             $e->setVerboseMessage((string) $eanError->verboseMessage);
             $e->setItineraryId((string) $eanError->itineraryId);
-            $e->setCode(400);
+            
+            /*
+             * 400 - Retry user input
+             * 490 - Restart booking flow
+             * 491 - Agent attention required
+             */
+            
+            $code = 490;
+            switch ($e->getHandling()) {
+                case 'RECOVERABLE':
+                    switch ($e->getCategory()) {
+                        case 'DATA_VALIDATION':
+                        $code = 400;
+                    }
+                    break;
+                case 'UNKNOWN':
+                case 'UNRECOVERABLE':
+                    $code = 490;
+                    break;
+                case 'AGENT_ATTENTION':
+                    $code = 491;
+                    break;
+            }
+            
+            $e->setCode($code);
 
             throw $e;
         }
